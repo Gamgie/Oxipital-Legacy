47324bad236a33f7fd06345d15d011e9ecbabc18
Gamgie <gamgie@pm.me>
2022-12-22 00:35:33 +0100

    orbGroupController expose the necessary to control orbs in chataigne. We have 2 orbController like a turntable A and B of a DJ

---
+++ Unity/HDRP_VFXGraph_Oxipital/Assets/Scripts/VFX/OrbGroupController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbGroupController : MonoBehaviour
+{
+    public int idControlled;
+    public int orbCount;
+
+    [Header("PS Parameters")]
+    [Range(0, 4000)]
+    public float rate;
+    [Range(0, 200)]
+    public float life;
+    [ColorUsage(true, true)]
+    public Color color;
+    public int colorIntensity;
+    [Range(0, 1)]
+    public float alpha;
+    [Range(0, 100)]
+    public float size;
+    public float drag;
+    [Range(0, 1)]
+    public float velocityDrag;
+
+    [Header("Emitter Parameters")]
+    public OrbGroup.EmitterShape emitterShape;
+    public OrbGroup.EmitterPlacementMode emitterPlacementMode;
+    public float emitterSize;
+    public bool emitFromInside;
+    public bool activateCollision;
+
+    private VFXController _vfxController;
+    private bool _isLinked;
+    private int _idControlled = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _vfxController = transform.parent.GetComponent<VFXController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Our id was updated then we need to update controller according to values
+        if(idControlled != _idControlled)
+		{
+            foreach (OrbGroup oG in _vfxController.orbs)
+            {
+                if (oG.orbGroupId == idControlled)
+                {
+                    rate = oG.rate;
+                    life = oG.life;
+                    color = oG.color;
+                    colorIntensity = oG.colorIntensity;
+                    alpha = oG.alpha;
+                    size = oG.size;
+                    drag = oG.drag;
+                    velocityDrag = oG.velocityDrag;
+                    emitterShape = oG.emitterShape;
+                    emitterPlacementMode = oG.emitterPlacementMode;
+                    emitterSize = oG.emitterSize;
+                    emitFromInside = oG.emitFromInside;
+                    activateCollision = oG.activateCollision;
+                }
+
+                _idControlled = idControlled;
+            }
+        }
+        else
+		{
+            foreach (OrbGroup oG in _vfxController.orbs)
+            {
+                if (oG.orbGroupId == idControlled)
+                {
+                    oG.rate = rate;
+                    oG.life = life;
+                    oG.color = color;
+                    oG.colorIntensity = colorIntensity;
+                    oG.alpha = alpha;
+                    oG.size = size;
+                    oG.drag = drag;
+                    oG.velocityDrag = velocityDrag;
+                    oG.emitterShape = emitterShape;
+                    oG.emitterPlacementMode = emitterPlacementMode;
+                    oG.emitterSize = emitterSize;
+                    oG.emitFromInside = emitFromInside;
+                    oG.activateCollision = activateCollision;
+                }
+            }
+        }
+    }
+}
