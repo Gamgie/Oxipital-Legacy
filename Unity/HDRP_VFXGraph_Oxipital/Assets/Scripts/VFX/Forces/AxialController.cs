9ad0e0a7e24bd9ebb4d1e1a114a1ec858cac89b6
Gamgie <gamgie@pm.me>
2023-04-28 01:11:08 +0200

    ballet integrated to axial force

--- Unity/HDRP_VFXGraph_Oxipital/Assets/Scripts/VFX/Forces/AxialController.cs
+++ Unity/HDRP_VFXGraph_Oxipital/Assets/Scripts/VFX/Forces/AxialController.cs
@@ -6,6 +6,8 @@ using UnityEngine.VFX;
 
 public class AxialController : ForceController
 {
+    protected readonly int s_BufferID = Shader.PropertyToID("Axial Graphics Buffer");
+
     // Update is called once per frame
     protected override void Update()
     {
@@ -18,9 +20,17 @@ public class AxialController : ForceController
             if (visualEffect.HasFloat(axial + " Intensity" + m_suffix))
                 visualEffect.SetFloat(axial + " Intensity" + m_suffix, intensity);
 
+            // Radius
+            if (visualEffect.HasFloat(axial + " Radius" + m_suffix))
+                visualEffect.SetFloat(axial + " Radius" + m_suffix, radius);
+
             // Axis
             if (visualEffect.HasVector3(axial + " Axis" + m_suffix))
                 visualEffect.SetVector3(axial + " Axis" + m_suffix, axis);
+
+            // Buffer
+            if (visualEffect.HasGraphicsBuffer(s_BufferID))
+                visualEffect.SetGraphicsBuffer(s_BufferID, m_buffer);
         }
     }
 }
