f7db2b6d88d0f7e222156167d1d76ec7465ee18b
Gamgie <gamgie@pm.me>
2023-05-04 21:07:19 +0200

    fix a bug when only one element in ballet line movment

--- Unity/HDRP_VFXGraph_Oxipital/Assets/Scripts/Ballet/BalletPattern.cs
+++ Unity/HDRP_VFXGraph_Oxipital/Assets/Scripts/Ballet/BalletPattern.cs
@@ -303,7 +303,7 @@ public class BalletPattern : MonoBehaviour
 
         if(dancerCount == 1)
 		{
-            linePositions[0] = new Vector3(0,Mathf.Sin(2 * Mathf.PI + currentSpeed + phase * Mathf.PI * 2),0);
+            linePositions[0] = new Vector3(0, currentSize * Mathf.Sin(2 * Mathf.PI * currentSpeed + phase * Mathf.PI * 2),0);
             linePositions[0] = transform.TransformPoint(linePositions[0]);
             return;
         }
@@ -312,7 +312,7 @@ public class BalletPattern : MonoBehaviour
         for (int i = 0; i < dancers.Count; i++)
         {
             linePositions[i] = new Vector3(0,
-                                           (i * currentSize * Mathf.Sin(2*Mathf.PI* currentSpeed + phase * Mathf.PI * 2) / (dancers.Count - 1)),
+                                           (i * currentSize * Mathf.Sin(2*Mathf.PI * currentSpeed + phase * Mathf.PI * 2) / (dancers.Count - 1)),
                                            0);
 
             linePositions[i] = transform.TransformPoint(linePositions[i]);
