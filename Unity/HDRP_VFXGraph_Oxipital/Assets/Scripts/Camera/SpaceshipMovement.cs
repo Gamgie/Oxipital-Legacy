ff8787a096f6ab37a71a7675dac9c944fb519013
Gamgie <gamgie@pm.me>
2023-06-08 18:17:17 +0200

    spaceship movement

--- Unity/HDRP_VFXGraph_Oxipital/Assets/Scripts/Camera/SpaceshipMovement.cs
+++ Unity/HDRP_VFXGraph_Oxipital/Assets/Scripts/Camera/SpaceshipMovement.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class SpaceshipMovement : CameraMovement
 {
+	public float thrust;
+	public float thrust1D;
+
+	public float yawTorque;
+	public float yaw1D;
+
+	public float pitchTorque;
+	public float pitch1D;
+
+	public float rollTorque;
+	public float roll1D;
+
 	public override void Init()
 	{
 		base.Init();
@@ -12,7 +24,25 @@ public class SpaceshipMovement : CameraMovement
 
 	public override bool UpdateMovement()
 	{
-		return base.UpdateMovement();
+		if (!base.UpdateMovement())
+			return false;
+
+		if(_rigidbody != null)
+		{
+			// Thrust
+			_rigidbody.AddRelativeForce(Vector3.forward * thrust * thrust1D * Time.deltaTime, ForceMode.Force);
+
+			// Roll
+			_rigidbody.AddRelativeTorque(Vector3.back * roll1D * rollTorque * Time.deltaTime);
+
+			// Pitch
+			_rigidbody.AddRelativeTorque(Vector3.left * pitch1D * pitchTorque * Time.deltaTime);
+
+			// Yaw
+			_rigidbody.AddRelativeTorque(Vector3.up * yaw1D * yawTorque * Time.deltaTime);
+		}
+
+		return true;
 	}
 
 	public override void Reset(float duration)
