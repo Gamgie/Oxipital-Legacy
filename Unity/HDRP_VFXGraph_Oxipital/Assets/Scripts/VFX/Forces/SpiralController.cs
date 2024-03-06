0b4b9677cafaa9e818dbaaa43bc20af34ce98761
Gamgie <gamgie@pm.me>
2024-03-06 07:26:39 +0100

    Spiral is now working correctly on each axis. Youhou !

--- Unity/HDRP_VFXGraph_Oxipital/Assets/Scripts/VFX/Forces/SpiralController.cs
+++ Unity/HDRP_VFXGraph_Oxipital/Assets/Scripts/VFX/Forces/SpiralController.cs
@@ -20,23 +20,11 @@ public class SpiralController : ForceController
     {
         base.Update();
 
-        // Compute Ax and Az
-        float Ax = Vector3.Angle(Vector3.up, new Vector3(axis.x, 0, 0)) * Mathf.PI / 180;
-        float Az = Vector3.Angle(Vector3.up, new Vector3(0, 0, axis.z)) * Mathf.PI / 180;
-
         foreach (VisualEffect visualEffect in _vfxs)
         {
             // Frequency
             if (visualEffect.HasFloat(key + " Frequency" + _suffix))
                 visualEffect.SetFloat(key + " Frequency" + _suffix, frequency);
-
-            // Ax
-            if (visualEffect.HasFloat(key + " Ax" + _suffix))
-                visualEffect.SetFloat(key + " Ax" + _suffix, Ax);
-
-            // Az
-            if (visualEffect.HasFloat(key + " Az" + _suffix))
-                visualEffect.SetFloat(key + " Az" + _suffix, Az);
 
             // Alpha
             if (visualEffect.HasFloat(key + " Alpha" + _suffix))
